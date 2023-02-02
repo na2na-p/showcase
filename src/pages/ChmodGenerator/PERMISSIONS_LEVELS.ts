078b9c11d09bdd6337482c6fe0e83b159ddfda8f
na2na-p <na2na@na2na.dev>
2023-02-02 22:12:02 +0900

    use PERMISSIONS_LEVEL

--- src/pages/ChmodGenerator/PERMISSIONS_LEVELS.ts
+++
@@ -1,7 +0,0 @@
-const PERMISSIONS_LEVELS = {
-	read: 4,
-	write: 2,
-	execute: 1
-} as const;
-
-export default PERMISSIONS_LEVELS;
---
+++ src/pages/ChmodGenerator/PERMISSION_LEVELS.ts
@@ -0,0 +1,7 @@
+const PERMISSION_LEVELS = {
+	read: 4,
+	write: 2,
+	execute: 1
+} as const;
+
+export default PERMISSION_LEVELS;
