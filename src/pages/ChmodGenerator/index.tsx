30aa262046abf19cf57924a908bb29e75518348e
na2na-p <na2na@na2na.dev>
2023-02-02 10:36:00 +0900

    Add chmod gen

--- src/pages/ChmodGenerator/index.tsx
+++ src/pages/ChmodGenerator/index.tsx
@@ -1,14 +1,20 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 import Table from '@/components/dataDisplay/Table';
 import Checkbox from '@/components/input/Checkbox';
 import Stack from '@/components/layout/Stack';
 import Typography from '@/components/layout/Typography';
+import { useHooks } from './hooks';
 
 const chmodGenerator = () => {
+	const { headers, bodies } =
+		useHooks(Checkbox);
+	// TODO: chmod逆引きするやつ
 	return (
 		<>
-			<Typography variant='h5'>chmodジェネレータ</Typography>
+			<Typography variant="h5">chmodジェネレータ</Typography>
+			{/* TextInput生やして逆引き用のあれこれする */}
+			<Table headers={headers} rows={bodies} />
 		</>
 	);
 };
