af8b0915f83f2ba3839a2a7e87d9e109a5823bef
na2na-p <na2na@na2na.dev>
2023-02-10 10:15:39 +0900

    Add dependencies, rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,28 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
+	env: {
+		browser: true,
+		es2021: true
 	},
-	'extends': [
-		'plugin:react/recommended',
-		'google'
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
+	extends: ['plugin:react/recommended', 'google'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
 		},
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
+		ecmaVersion: 'latest',
+		sourceType: 'module'
 	},
-	'plugins': [
+	plugins: [
 		'react',
 		'import',
 		'react-hooks',
 		'react-refresh',
+		'prettier',
 		'@typescript-eslint'
 	],
-	'rules': {
+	rules: {
 		'react/react-in-jsx-scope': 'off',
-		'react/no-unknown-property': [
-			'error',
-			{ ignore: ['css'] }
-		],
+		'react/no-unknown-property': ['error', { ignore: ['css'] }],
 		'require-jsdoc': 'off',
 		'react/display-name': 'off',
 		'no-tabs': 'off',
@@ -52,34 +47,34 @@ module.exports = {
 				],
 				'pathGroups': [
 					{
-						'pattern': '@/**',
-						'group': 'external',
-						'position': 'after'
+						pattern: '@/**',
+						group: 'external',
+						position: 'after'
 					}
 				],
 				'alphabetize': {
-					'order': 'asc',
-					'caseInsensitive': true
+					order: 'asc',
+					caseInsensitive: true
 				},
 				'newlines-between': 'always'
 			}
 		],
 		'@typescript-eslint/consistent-type-imports': [
 			'error',
-			{ 'prefer': 'type-imports' }
+			{ prefer: 'type-imports' }
 		],
 		'import/no-duplicates': 'error',
 		'@typescript-eslint/naming-convention': [
 			'error',
 			{
-				'selector': 'typeAlias',
-				'format': ['PascalCase']
+				selector: 'typeAlias',
+				format: ['PascalCase']
 			}
 		]
 	},
-	'settings': {
-		'react': {
-			'version': 'detect'
+	settings: {
+		react: {
+			version: 'detect'
 		}
 	}
 };
